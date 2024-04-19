1f6519498678997bd0f77104de57b10641eeb2ce
JNeubau <neubauer.asia@gmail.com>
2024-04-19 11:57:00 +0200

    19.04.24 lab

---
+++ MvcNews/MvcNews/Migrations/20240419085937_AddRowVersionMig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcNews.Migrations
+{
+    public partial class AddRowVersionMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(140)",
+                maxLength: 140,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(140)",
+                oldMaxLength: 140);
+        }
+    }
+}
